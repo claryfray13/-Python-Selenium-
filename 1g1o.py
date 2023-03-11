23a8d45c83d93267a14567cabec3dd2760c397f1
claryfray13 <120520342+claryfray13@users.noreply.github.com>
2023-03-11 15:35:15 +0300

    Create 1g1o.py

---
+++ 1g1o.py
@@ -0,0 +1,27 @@
+#1.GÜN
+#ÖDEV1
+"""
+En çok kullanılan veri tiplerini aşağıdaki gibi tanımlayabiliriz
+-String -> metinsel ifadeler. tek tırnak veya çift tırnak içinde ifade edilir. 
+-Sayısal -> int ,float ve complex olarak ayırabiliriz. int tam sayı değerleri tutar.
+float ondalık sayıları tutar. complex karmaşık sayıları tutar.
+-Boolean -> 0 ve 1 gibi en basit olan değeri tutar. mantıksal sonuç da diyebiliriz. true ve false.
+-List -> içinde birden fazla veri tutar. veri tipleri aynı olmak zorunda değildir. veri tekrarına izin verir.
+-Set -> listenin aksine matematikteki küme kavramı gibi tekrara izin vermez. özgüdür.
+-Tuple -> listenin değiştirelemez halidir.
+
+
+Web sayfasında gördüğümüz her metinsel ifade stringtir.
+ilk başta göze int gibi gelen 25% ifadesi de bir stringtir.
+2023 yazısı integer.
+Derslerin başındaki maddeler boolean olarak kodlanmıştır.
+eğer ders biterse yani değerimiz doğrusa içi boyanacaktır.
+ders bitmediyse içi boş kalacaktır. false'tur.
+
+uygulamaya giriş yaparken e-mail ve şifre soruluyor.
+eğer girdiğim input değerler veritabanında eşleşirse benim sisteme girmeme izin verir.
+eğer eşleşmesse hata verir.
+
+
+
+"""
